0447f79066db4abab6b5de7def7766d846afaff6
Dehemi-Perera <lilyvihara07@gmail.com>
2025-02-04 18:54:31 +0530

    initate project

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  function incrementCount() {
+    setCount(function (prevCount) {
+      return prevCount + 1;
+    });
+  }
+
+  function decreaseCount(){
+    setCount(function(prevCount){
+      return prevCount-1;
+    });
+  }
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={incrementCount}>
+          increasing count 
+        </button><br></br>
+        <button onClick={decreaseCount}>
+          decreasing count 
+        </button>
+        <h1>
+          {count}
+        </h1>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
