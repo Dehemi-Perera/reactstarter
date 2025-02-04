b5995ef6ddcf4a3388efc09691cbd9c14b4208c6
Dehemi-Perera <lilyvihara07@gmail.com>
2025-02-04 22:32:27 +0530

    initate project

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,19 @@ function App() {
       return prevCount-1;
     });
   }
+  function resetCount() {
+    setCount(0);
+  }
+
+  function checkOddEven(){
+    if (count%2===0)
+      return "even";
+    else
+      return "odd";
+  }
+
+
+
 
 
   return (
@@ -37,9 +50,15 @@ function App() {
         </button><br></br>
         <button onClick={decreaseCount}>
           decreasing count 
+        </button><br></br>
+        <button onClick={resetCount}>
+          reset 
         </button>
         <h1>
           {count}
+        </h1>
+        <h1>
+          {checkOddEven()}
         </h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
